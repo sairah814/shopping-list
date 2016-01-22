4753eb3613ab9797dc9df47bf95b6ba3c38c4481
sairah814 <sairah814@gmail.com>
2016-01-21 22:38:07 -0500

    jQuery for adding item to list

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 /*jslint browser: true*/
 /*global $, jQuery, alert*/
 
-//On pressing enter you add the item to a list underneath
+// On pressing enter you add the item to a list underneath
 $(document).ready(function () {
     function getItem() {
         $(document).keydown(function (key) {
@@ -11,17 +11,27 @@ $(document).ready(function () {
             }
         });
     }
-
     getItem();
 
-    function itemAdder() {
-        /*$(document).keydown(function (key) {
-            if (key.keyCode == 13) {*/
-        var newitem = $('#add-item').val();
-        $('#active-list').prepend("<li>" + newitem + "</li>");
-        /*}
-        });*/
-    }
-});
+    var styleunchecked = '<span class="unchecked"><i class="fa fa-circle-o"></i></span>';
+    var styleremove = '<span class="remove"><i class="fa fa-remove"></i></span>';
+    var stylechecked = '';
 
-//2. If you remove the item, it is removed from the list
+    function itemAdder() {
+        var newitem = $('#add-item').val();
+        $('#active-list').prepend("<li>" + styleunchecked + newitem + styleremove + "</li>");
+    }
+    // If you check an item, it is moved to 'Done'
+    $(document).on('click', '.unchecked', function () {
+        var $moveitem = $(this);
+        $(this).closest('li').remove();
+        $('#done-list').prepend("<li>" + stylechecked + $moveitem + "</li")
+    });
+
+    // If you remove the item, it is removed from the list
+    $(document).on('click', '.remove', function () {
+        $(this).closest('li').remove();
+    });
+
+    // If you click clear all then all items are removed from the 'Done' list
+});
